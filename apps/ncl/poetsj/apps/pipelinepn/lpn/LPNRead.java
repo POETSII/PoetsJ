2c370fe638daae5f30c925eafee950bddc804ee9
Ashur Rafiev <ashurrafiev@users.noreply.github.com>
2021-07-12 12:25:07 +0100

    LPN converter

---
+++ apps/ncl/poetsj/apps/pipelinepn/lpn/LPNRead.java
@@ -0,0 +1,206 @@
+package ncl.poetsj.apps.pipelinepn.lpn;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LPNRead {
+
+	public static final String path = "../Poets.Temp/pipeline_tile.lpn";
+	
+	public static class OutMap {
+		public int dev;
+		public Place place;
+	}
+	
+	public static class Place implements Comparable<Place> {
+		public int index;
+		public String name;
+		public boolean marked = false;
+		public boolean choice = false;
+		public OutMap out = null;
+		@Override
+		public String toString() {
+			String s = String.format("P[%d] %s%s", index, marked ? "*" : "", name);
+			if(out!=null)
+				s += String.format(" (out%+d:%s)", out.dev, out.place.name);
+			return s;
+		}
+		@Override
+		public int compareTo(Place o) {
+			int res = Boolean.compare(this.out!=null, o.out!=null);
+			if(res==0)
+				res = compareName(this.name, o.name);
+			return res;
+		}
+	}
+
+	public static class Transition implements Comparable<Transition> {
+		public String name;
+		public ArrayList<Place> in = new ArrayList<>();
+		public ArrayList<Place> out = new ArrayList<>();
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder("T ");
+			sb.append(name);
+			sb.append(" in:{");
+			for(Place p : in) {
+				sb.append(p.name);
+				sb.append(";");
+			}
+			sb.append("} out:{");
+			for(Place p : out) {
+				sb.append(p.name);
+				sb.append(";");
+			}
+			sb.append("}");
+			return sb.toString();
+		}
+		@Override
+		public int compareTo(Transition o) {
+			return compareName(this.name, o.name);
+		}
+	}
+
+	public HashMap<String, Place> placeMap = new HashMap<>();
+	public HashMap<String, Transition> transitionMap = new HashMap<>();
+
+	public ArrayList<Place> places;
+	public ArrayList<Transition> transitions;
+	public int localPlaces;
+	public int outPlaces;
+	
+	public static int getNamePrefix(String n) {
+		return n.startsWith("ccw_") ? -1 : n.startsWith("cw_") ? 1 : 0;
+	}
+	
+	public static int compareName(String n1, String n2) {
+		int res = Integer.compare(getNamePrefix(n1), getNamePrefix(n2));
+		if(res==0)
+			n1.compareTo(n2);
+		return res;
+	}
+	
+	public Place getPlaceByName(String name) {
+		Place p = placeMap.get(name);
+		if(p==null) {
+			p = new Place();
+			p.index = placeMap.size();
+			p.name = name;
+			placeMap.put(name, p);
+		}
+		return p;
+	}
+	
+	public LPNRead load(String path) {
+		try {
+			Scanner in = new Scanner(new FileInputStream(new File(path)));
+			
+			int[] reorder = null;
+			
+			while(in.hasNext()) {
+				String line = in.nextLine();
+				if(line.isEmpty() || line.startsWith("#"))
+					continue;
+				String[] s = line.split("\\s+");
+				for(int i=0; i<s.length; i++) {
+					if(s[i].startsWith("{"))
+						s[i] = s[i].substring(1);
+					if(s[i].endsWith("}"))
+						s[i] = s[i].substring(0, s[i].length()-1);
+				}
+				if(s[0].startsWith(".")) {
+					if(s[0].equals(".dummy")) {
+						for(int i=1; i<s.length; i++) {
+							Transition t = new Transition();
+							t.name = s[i];
+							transitionMap.put(t.name, t);
+						}
+					}
+					else if(s[0].equals(".marking")) {
+						for(int i=1; i<s.length; i++) {
+							Place p = placeMap.get(s[i]);
+							p.marked = true;
+						}
+					}
+					else if(s[0].equals(".out")) {
+						Place p = placeMap.get(s[1]);
+						p.out = new OutMap();
+						
+						if(s[2].equals("ccw"))
+							p.out.dev = -1;
+						else if(s[2].equals("cw"))
+							p.out.dev = 1;
+						else
+							throw new RuntimeException();
+						p.out.place = placeMap.get(s[3]);
+					}
+					else if(s[0].equals(".reorder")) {
+						reorder = new int[s.length-1];
+						for(int i=1; i<s.length; i++) {
+							reorder[i-1] = Integer.parseInt(s[i]);
+						}
+					}
+				}
+				else {
+					Transition t = transitionMap.get(s[0]);
+					if(t!=null) {
+						for(int i=1; i<s.length; i++)
+							t.out.add(getPlaceByName(s[i]));
+					}
+					else {
+						Place p = getPlaceByName(s[0]);
+						for(int i=1; i<s.length; i++)
+							transitionMap.get(s[i]).in.add(p);
+						if(s.length>2)
+							p.choice = true;
+					}
+				}
+			}
+			
+			places = new ArrayList<>(placeMap.values());
+			places.sort(null);
+			localPlaces = 0;
+			outPlaces = 0;
+			int index = 0;
+			for(Place p : places) {
+				if(p.out==null)
+					localPlaces++;
+				else
+					outPlaces++;
+				p.index = index++;
+			}
+			
+			transitions = new ArrayList<>(transitionMap.values());
+			transitions.sort(null);
+			if(reorder!=null) {
+				ArrayList<Transition> treorder = new ArrayList<>(transitions.size());
+				for(int i=0; i<transitions.size(); i++)
+					treorder.add(transitions.get(reorder[i]));
+				transitions = treorder;
+			}
+			
+			in.close();
+			return this;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void print() {
+		for(Place p : places) {
+			System.out.println(p);
+		}
+		for(Transition t : transitions)
+			System.out.println(t);
+	}
+	
+	public static void main(String[] args) {
+		new LPNRead().load(path).print();
+	}
+
+}
---
+++ apps/ncl/poetsj/apps/pipelinepn/lpn/LpnToPpn.java
@@ -0,0 +1,199 @@
+package ncl.poetsj.apps.pipelinepn.lpn;
+
+import static java.lang.System.out;
+
+import ncl.poetsj.apps.pipelinepn.lpn.LPNRead.Place;
+import ncl.poetsj.apps.pipelinepn.lpn.LPNRead.Transition;
+
+public class LpnToPpn {
+
+	public static int transitionAlign = 1;
+	
+	public static void printTmapJava(LPNRead lpn, int steps) {
+		out.println("// Places:");
+		for(Place p : lpn.places) {
+			out.printf("// %d%s: %s%s\n", p.index, p.marked ? "*" : "", p.name, p.out==null ? "" : " (out)");
+		}
+		
+		out.printf("localPlaces = %d;\noutPlaces = %d;\n\n", lpn.localPlaces*steps, lpn.outPlaces);
+
+		out.printf("// Initial marking: \ninitPlaces = new int[] {");
+		for(int pi=0; pi<lpn.localPlaces; pi++) {
+			if(pi>0) out.print(", ");
+			out.print(lpn.places.get(pi).marked ? 1 : 0);
+		}
+		out.printf("};\ntileInitPlaces = %d;\n", lpn.localPlaces);
+		
+		out.printf("\n// Output mapping:\nint offs = 0;\ntOutMap = new OutMap[outPlaces];\n");
+		for(int pi=0; pi<lpn.outPlaces; pi++) {
+			Place p = lpn.places.get(pi+lpn.localPlaces);
+			out.printf("tOutMap[offs++] = new OutMap(%d, %d);\n", p.out.dev, p.out.dev<0 ? lpn.localPlaces*(steps-1) + p.out.place.index : p.out.place.index);
+		}
+
+		int tsize = lpn.transitions.size();
+		int transitions = ((tsize+(transitionAlign-1)) / transitionAlign) * transitionAlign;
+		out.printf("\n// Transitions:\noffs = 0;\ntransitions = %d; // (%d aligned to %d) x %d steps\n"+
+				"tmap = new Transition[transitions];\n\n// {0xoi, inp-s, outp-s}\n",
+				transitions*steps, lpn.transitions.size(), transitionAlign, steps);
+		
+		for(int s=0; s<steps; s++) {
+			for(int ti=0; ti<transitions; ti++) {
+				if(ti>tsize) {
+					out.println("tmap[offs++] = new Transition(0);");
+				}
+				else {
+					int p0 = lpn.localPlaces*s;
+					Transition t = lpn.transitions.get(ti);
+					out.printf("tmap[offs++] = new Transition(0x%d%d", t.out.size(), t.in.size());
+					boolean choice = false;
+					for(Place p : t.in) {
+						out.printf(", %d", p0 + p.index);
+						if(p.choice)
+							choice = true;
+					}
+					for(Place p : t.out) {
+						if(p.out!=null) {
+							if(p.out.dev<0 && s>0)
+								out.printf(", %d /* prev */ ", p0 - lpn.localPlaces + p.out.place.index);
+							else if(p.out.dev>0 && s<steps-1)
+								out.printf(", %d /* next */ ", p0 + lpn.localPlaces + p.out.place.index);
+							else
+								out.printf(", %d /* out */ ", lpn.localPlaces*(steps-1) + p.index);
+						}
+						else {
+							out.printf(", %d", p0 + p.index);
+						}
+					}
+					out.printf("); // %d: %s%s\n", ti, t.name, choice ? " *choice" : "");
+				}
+				if(transitionAlign>1 && ti%transitionAlign==transitionAlign-1)
+					out.println();
+			}
+		}
+	}
+
+	public static void printTmapGenC(LPNRead lpn) {
+		int tsize = lpn.transitions.size();
+		int transitions = ((tsize+(transitionAlign-1)) / transitionAlign) * transitionAlign;
+		out.printf("void generateTMap(uint32_t steps) {\n"+
+				"\tnumPlaces = steps*%d;\n"+
+				"\ttransitions = steps*%d;\n"+
+				"\ttmap = new Transition[transitions];\n\n"+
+				"\tuint32_t offs = 0;\n\tuint32_t p = 0;\n"+
+				"\tfor(uint32_t s = 0; s<steps; s++) {\n", lpn.localPlaces, transitions);
+		
+		for(int ti=0; ti<transitions; ti++) {
+			if(ti>tsize) {
+				out.println("{0},");
+			}
+			else {
+				//int p0 = lpn.localPlaces*s;
+				Transition t = lpn.transitions.get(ti);
+				out.printf("\t\ttmap[offs++] = {0x%d%d", t.out.size(), t.in.size());
+				boolean choice = false;
+				for(Place p : t.in) {
+					out.printf(", p+%d", p.index);
+					if(p.choice)
+						choice = true;
+				}
+				for(Place p : t.out) {
+					if(p.out!=null) {
+						if(p.out.dev<0)
+							out.printf(", (s>0 ? p-%d : (steps-1)*%d) + %d", lpn.localPlaces, lpn.localPlaces, p.out.place.index);
+						else if(p.out.dev>0)
+							out.printf(", (s<steps-1 ? p+%d : 0) + %d", lpn.localPlaces, p.out.place.index);
+					}
+					else {
+						out.printf(", p+%d", p.index);
+					}
+				}
+				out.printf("}; // %d: %s%s\n", ti, t.name, choice ? " *choice" : "");
+				if(ti%transitionAlign==transitionAlign-1)
+					out.println();
+			}
+		}
+		
+		out.printf("\t\tp += %d;\n\t}\n}", lpn.localPlaces);
+	}
+
+	public static void printTmapC(String prefix, LPNRead lpn, int steps) {
+		out.printf("#ifndef _%s_TMAP_H_\n#define _%s_TMAP_H_\n\n", prefix, prefix);
+		out.printf("const uint16_t localPlaces = %d;\nconst uint8_t outPlaces = %d;\n", lpn.localPlaces*steps, lpn.outPlaces);
+		
+		int tsize = lpn.transitions.size();
+		int transitions = ((tsize+(transitionAlign-1)) / transitionAlign) * transitionAlign;
+		out.printf("const uint16_t transitions = %d; // (%d aligned to %d) x %d steps\n\n"+
+				"const Transition tmap[transitions] = {\n\t// {0xoi, inp-s, outp-s}\n",
+				transitions*steps, lpn.transitions.size(), transitionAlign, steps);
+		
+		for(int s=0; s<steps; s++) {
+			for(int ti=0; ti<transitions; ti++) {
+				String comma = (s==steps-1 && ti==transitions-1) ? "" : ",";
+				if(ti>tsize) {
+					out.println("{0},");
+				}
+				else {
+					int p0 = lpn.localPlaces*s;
+					Transition t = lpn.transitions.get(ti);
+					out.printf("\t{0x%d%d", t.out.size(), t.in.size());
+					boolean choice = false;
+					for(Place p : t.in) {
+						out.printf(", %d", p0 + p.index);
+						if(p.choice)
+							choice = true;
+					}
+					for(Place p : t.out) {
+						if(p.out!=null) {
+							if(p.out.dev<0 && s>0)
+								out.printf(", %d /* prev */ ", p0 - lpn.localPlaces + p.out.place.index);
+							else if(p.out.dev>0 && s<steps-1)
+								out.printf(", %d /* next */ ", p0 + lpn.localPlaces + p.out.place.index);
+							else
+								out.printf(", %d /* out */ ", lpn.localPlaces*(steps-1) + p.index);
+						}
+						else {
+							out.printf(", %d", p0 + p.index);
+						}
+					}
+					out.printf("}%s // %d:%d: %s%s\n", comma, s, ti, t.name, choice ? " *choice" : "");
+				}
+				if(ti%transitionAlign==transitionAlign-1)
+					out.println();
+			}
+		}
+		out.println("};\n\n#endif");
+	}
+	
+	public static void printPInitC(String prefix, LPNRead lpn, int steps) {
+		out.printf("#ifndef _%s_PINIT_H_\n#define _%s_PINIT_H_\n\n", prefix, prefix);
+		out.println("// Places:");
+		for(Place p : lpn.places) {
+			out.printf("// %d%s: %s%s\n", p.index, p.marked ? "*" : "", p.name, p.out==null ? "" : " (out)");
+		}
+		
+		out.printf("\n// Initial marking: \nconst uint8_t initPlaces[localPlaces] = {");
+		for(int pi=0; pi<lpn.localPlaces; pi++) {
+			if(pi>0) out.print(", ");
+			out.print(lpn.places.get(pi).marked ? 1 : 0);
+		}
+		out.printf("};\nconst uint16_t tileInitPlaces = %d;\n", lpn.localPlaces);
+		
+		out.printf("\n// Output mapping:\nconst OutMap tOutMap[outPlaces] = {\n");
+		for(int pi=0; pi<lpn.outPlaces; pi++) {
+			String comma = (pi==lpn.outPlaces-1) ? "" : ",";
+			Place p = lpn.places.get(pi+lpn.localPlaces);
+			out.printf("\t{%d, %d}%s // %s to %s\n",
+					p.out.dev, p.out.dev<0 ? lpn.localPlaces*(steps-1) + p.out.place.index : p.out.place.index,
+					comma, p.name, p.out.place.name);
+		}
+		out.println("};\n\n#endif");
+	}
+
+	public static void main(String[] args) {
+		printTmapJava(new LPNRead().load(LPNRead.path), 2);
+		//printTmapGenC(new LPNRead().load(LPNRead.path));
+		//printTmapC("DPHIL", new LPNRead().load(LPNRead.path), 2);
+		//printPInitC("DPHIL", new LPNRead().load(LPNRead.path), 2);
+	}
+	
+}
